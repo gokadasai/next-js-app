96e79d842155cdbcd905d36967ca11f73aa76548
gokadasai <55623504+gokadasai@users.noreply.github.com>
2025-08-23 16:42:57 +0530

    build issues

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // 👈 tells Next.js to generate static files
-  images: { unoptimized: true }, // required if you use next/image
-  basePath: "/next-js-app",   // 👈 only needed if NOT deploying to username.github.io
-  assetPrefix: "/next-js-app",
+  output: "export",
 };
 
 export default nextConfig;

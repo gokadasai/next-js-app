df44a9b7ae6a35e8b4bdccd6e08bbf10a585ff01
gokadasai <55623504+gokadasai@users.noreply.github.com>
2025-08-23 16:51:40 +0530

    repo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export",              // for static export
+  basePath: "/next-js-app",      // 👈 repo name
+  assetPrefix: "/next-js-app/",  // 👈 ensures CSS/JS loads from correct path
 };
 
 export default nextConfig;
